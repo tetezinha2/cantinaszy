13d7122dc1531d17c2f1fd1e8dfb1fe0a3c6e305
tetezinha2 <estefany.braz@escola.pr.gov.br>
2025-09-18 11:34:10 -0300

    gfg

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+var nomeCantina="Cantina da Escola";
+console.log("Bem-vindo à" + Cantina da Escola);
+let salgados=20
+console.log("Temos" + salgados + "salgados disponíveis.");
+const precoSalgado=5;
+console.log("Cada salgado custa R$6,00" + precoSalgado);
